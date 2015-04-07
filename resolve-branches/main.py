27bc8aaaed480c5d3ef9399e35009c82d683e4f7
Garrett Kadillak <gkadillak@gmail.com>
2015-04-07 16:37:29 -0700

    Techcercises for Tuesday April 7th

---
+++ resolve-branches/main.py
@@ -0,0 +1,9 @@
+def say_hello(name):
+	"""Greet with parameter name"""
+	return 'Hello there, {:s}'.format(name)
+
+# The count-up branch will implements a function that will count up to a particular value
+
+# The branch pick-random branch will implement a function that will return a random element from a list
+
+# The branch reverse-name will implement a function that returns the reverse order of the name parameter
