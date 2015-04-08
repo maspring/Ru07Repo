fe9766fbf0d3c57cf7c16f4d067a59829a681675
Ben Havilland <ben@havilland.net>
2015-04-08 13:28:12 -0700

    Exercises for strings and hangman done.

---
+++ hangman/temperature_converter.py
@@ -0,0 +1,11 @@
+# Here is an example of how you would catch input from the user
+# To see this example in action, go to the command line and run
+# python temperature_converter.py
+# This will promp the user, the user types something, and then this is printed out
+user_input = raw_input("Please put in some input: ")
+
+print user_input
+
+def temperature_converter(temp):
+	"""Write the temperature_converter function body here"""
+	pass
